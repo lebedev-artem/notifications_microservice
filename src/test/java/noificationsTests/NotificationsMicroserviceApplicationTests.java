f67dc4131bef82e7db56016df676f7b00ff87f9c
Alexey Baryshev <for_leha@mail.ru>
2023-06-29 16:04:03 +0300

    Project for microservice

--- src/test/java/noificationsTests/NotificationsMicroserviceApplicationTests.java
+++ src/test/java/noificationsTests/NotificationsMicroserviceApplicationTests.java
@@ -1,10 +1,10 @@
-package noifications.example.demo;
+package noificationsTests;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class NotificationsMicroserviceApplicationTests {
 
 	@Test
 	void contextLoads() {
